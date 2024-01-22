ad57463d653e6a799e0c78999340754ee92a95cd
violetta <lotsmanovavioletta@gmail.com>
2024-01-22 20:06:50 +0300

    add models pipelines and steps

---
+++ src/pipeline/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import BigInteger, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from src.database import Base
+
+
+class Pipelines(Base):
+    """Модель паплайнов"""
+
+    __tablename__ = "pipeline"
+
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, index=True)
+    title: Mapped[str] = mapped_column()
+
+    steps = relationship("Steps", back_populates="pipeline")
+
+
+class Steps(Base):
+    """Модель шагов паплайна"""
+
+    __tablename__ = "step"
+
+    step_id: Mapped[int] = mapped_column(BigInteger, primary_key=True, index=True)
+    title: Mapped[str] = mapped_column()
+    step_parameters: Mapped[str] = mapped_column()
+
+    pipeline_id: Mapped[int] = mapped_column(BigInteger, ForeignKey("pipeline.id"), nullable=True)
+    pipeline = relationship("Pipelines", back_populates="steps")

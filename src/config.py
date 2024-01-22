aae1a5a2cdecb82b95e4d3ba4462c70661ea36fc
violetta <lotsmanovavioletta@gmail.com>
2024-01-22 20:06:32 +0300

    add settings

---
+++ src/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# setting for DB
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_NAME = os.environ.get("POSTGRES_DB")
+DB_USER = os.environ.get("POSTGRES_USER")
+DB_PASS = os.environ.get("POSTGRES_PASSWORD")

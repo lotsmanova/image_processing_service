459ef37266e92b0d8a2215da6b0ec564979bc8e7
violetta <lotsmanovavioletta@gmail.com>
2024-01-23 22:08:45 +0300

    add model ml

---
+++ model_machine_learning/detection_result/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import BigInteger
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.database import Base
+
+
+class Result(Base):
+    """Модель результата детекции"""
+
+    __tablename__ = "detection_result"
+
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, index=True)
+    processed_image: Mapped[str] = mapped_column()
+    is_detected: Mapped[bool] = mapped_column()
+    top_left_x: Mapped[float] = mapped_column(nullable=True)
+    top_left_y: Mapped[float] = mapped_column(nullable=True)
+    width: Mapped[float] = mapped_column(nullable=True)
+    height: Mapped[float] = mapped_column(nullable=True)
+    confidence: Mapped[float] = mapped_column(nullable=True)
+    label: Mapped[int] = mapped_column(nullable=True)
